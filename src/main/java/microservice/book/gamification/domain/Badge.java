d3db26072d9c10da67a7f9d7c250b2e79ba69cfb
sabaja <jacoposabatini76@gmail.com>
2018-11-15 23:04:49 +0100

    Added all domain classes

--- src/main/java/microservice/book/gamification/domain/Badge.java
+++ src/main/java/microservice/book/gamification/domain/Badge.java
@@ -1,14 +1,14 @@
 package microservice.book.gamification.domain;
 
+/**
+ * Enumeration with the different types of Badges that a user can win.
+ */
 public enum Badge {
 
 	// Badges depending on score
-	BRONZE_MULTIPLICATOR, 
-	SILVER_MULTIPLICATOR, 
-	GOLD_MULTIPLICATOR,
-	
+	BRONZE_MULTIPLICATOR, SILVER_MULTIPLICATOR, GOLD_MULTIPLICATOR,
+
 	// Other badges won for different conditions
-	FIRST_ATTEMPT, 
-	FIRST_WON;
-	
+	FIRST_ATTEMPT, FIRST_WON;
+
 }
--- src/main/java/microservice/book/gamification/domain/ScoreCard.java
+++ src/main/java/microservice/book/gamification/domain/ScoreCard.java
@@ -2,22 +2,56 @@ package microservice.book.gamification.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+/**
+ * This class represents the Score linked to an attempt in the game, with an
+ * associated user and the timestamp in which the score is registered.
+ */
 @RequiredArgsConstructor
 @Getter
-@EqualsAndHashCode
 @ToString
+@EqualsAndHashCode
 @Entity
-@Table(name = "SCORE_CARD")
-public class ScoreCard implements Serializable{
+public final class ScoreCard implements Serializable {
 
-	private static final long serialVersionUID = -2454646969873103024L;
-	
+	private static final long serialVersionUID = -8110821707173595409L;
+
+	// The default score assigned to this card, if not specified.
+	public static final int DEFAULT_SCORE = 10;
+
+	@Id
+	@GeneratedValue
+	@Column(name = "CARD_ID")
+	private final Long cardId;
+
+	@Column(name = "USER_ID")
+	private final Long userId;
+
+	@Column(name = "ATTEMPT_ID")
+	private final Long attemptId;
+
+	@Column(name = "SCORE_TS")
+	private final long scoreTimestamp;
+
+	@Column(name = "SCORE")
+	private final int score;
+
+	// Empty constructor for JSON / JPA
+	public ScoreCard() {
+		this(null, null, null, 0, 0);
+	}
+
+	public ScoreCard(final Long userId, final Long attemptId) {
+		this(null, userId, attemptId, System.currentTimeMillis(), DEFAULT_SCORE);
+	}
+
 }

87c233aff788cf15a0840cf08d72a54deb8515b2
sabaja <jacoposabatini76@gmail.com>
2018-11-23 11:08:25 +0100

    bug fix - requestmapping for LeaderBoardController

--- src/main/java/microservice/book/gamification/controller/LeaderBoardController.java
+++ src/main/java/microservice/book/gamification/controller/LeaderBoardController.java
@@ -3,6 +3,7 @@ package microservice.book.gamification.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import microservice.book.gamification.domain.LeaderBoardRow;
 import microservice.book.gamification.service.LeaderBoardService;
 
 @RestController
+@RequestMapping(value = "/leaders")
 public class LeaderBoardController {
 
 	private final LeaderBoardService leaderBoardService;
@@ -19,15 +21,15 @@ public class LeaderBoardController {
 		this.leaderBoardService = leaderBoardService;
 	}
 
-	@GetMapping(value = "/leaders")
+	@GetMapping
 	public List<LeaderBoardRow> leaders() {
 		return this.leaderBoardService.getCurrentLeaderBoard();
 	}
 
-	@GetMapping(value = "/leaders")
+	@GetMapping(value = "/pages")
 	public List<LeaderBoardRow> leaders(@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "size", defaultValue = "1") Integer size) {
-		return this.leaderBoardService.getCurrentLeaderBoard();
+			@RequestParam(value = "size", defaultValue = "1") Integer size) throws Exception {
+		return this.leaderBoardService.getCurrentLeaderBoard(page, size);
 	}
 
 }

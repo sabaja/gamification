43144983344498a79aec22bfac5489b315805b99
sabaja <jacoposabatini76@gmail.com>
2018-11-23 15:54:56 +0100

    Add MultiplicationSolvedEvent

---
+++ src/main/java/microservice/book/gamification/event/MultiplicationSolvedEvent.java
@@ -0,0 +1,26 @@
+package microservice.book.gamification.event;
+
+import java.io.Serializable;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Event that models the fact that a {@link microservices.book.multiplication.domain.Multiplication}
+ * has been solved in the system. Provides some context information about the multiplication.
+ */
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public class MultiplicationSolvedEvent implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private final Long multiplicationResultAttemptId;
+	private final Long userId;
+	private final boolean correct;
+	
+}

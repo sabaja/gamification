26b9d7d0b9721caec9c1a8116c763592228a2738
sabaja <jacoposabatini76@gmail.com>
2019-01-04 17:33:23 +0100

    minor changes

--- src/main/java/microservices/book/gamification/event/EventHandler.java
+++ src/main/java/microservices/book/gamification/event/EventHandler.java
@@ -7,8 +7,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 /**
- * This class receives the events and triggers the associated
- * business logic.
+ * This class receives the events and triggers the associated business logic.
  */
 @Slf4j
 @Component
@@ -16,21 +15,21 @@ public class EventHandler {
 
 	private GameService gameService;
 
-    EventHandler(final GameService gameService) {
-        this.gameService = gameService;
-    }
+	EventHandler(final GameService gameService) {
+		this.gameService = gameService;
+	}
 
-    @RabbitListener(queues = "${multiplication.queue}")
-    void handleMultiplicationSolved(final MultiplicationSolvedEvent event) {
-        log.info("Multiplication Solved Event received: {}", event.getMultiplicationResultAttemptId());
-        try {
-            gameService.newAttemptForUser(event.getUserId(),
-                    event.getMultiplicationResultAttemptId(),
-                    event.isCorrect());
-        } catch (final Exception e) {
-            log.error("Error when trying to process MultiplicationSolvedEvent", e);
-            // Avoids the event to be re-queued and reprocessed.
-            throw new AmqpRejectAndDontRequeueException(e);
-        }
-    }
+	@RabbitListener(queues = "${multiplication.queue}")
+	void handleMultiplicationSolved(final MultiplicationSolvedEvent event) {
+		log.info("Multiplication Solved Event received id: {}, user: {}, correct: {}",
+				event.getMultiplicationResultAttemptId(), event.getUserId(), event.isCorrect());
+		try {
+			gameService.newAttemptForUser(event.getUserId(), event.getMultiplicationResultAttemptId(),
+					event.isCorrect());
+		} catch (final Exception e) {
+			log.error("Error when trying to process MultiplicationSolvedEvent", e);
+			// Avoids the event to be re-queued and reprocessed.
+			throw new AmqpRejectAndDontRequeueException(e);
+		}
+	}
 }

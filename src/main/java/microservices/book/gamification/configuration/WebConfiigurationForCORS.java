298b5b29b92493d00693aace0292824649e8000c
sabaja <jacoposabatini76@gmail.com>
2019-02-02 16:46:20 +0100

    Add WebConfig for CORS

--- src/main/java/microservices/book/gamification/configuration/WebConfiigurationForCORS.java
+++ src/main/java/microservices/book/gamification/configuration/WebConfiigurationForCORS.java
@@ -10,19 +10,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * https://www.logicbig.com/how-to/code-snippets/jcode-spring-mvc-webmvcconfigurer.html
- 
-     * Enables Cross-Origin Resource Sharing (CORS)
-     * More info: http://docs.spring.io/spring/docs/current/spring-framework-reference/html/cors.html
-     * @param registry
-  
+ * 
+ * Enables Cross-Origin Resource Sharing (CORS) More info:
+ * http://docs.spring.io/spring/docs/current/spring-framework-reference/html/cors.html
+ * 
+ * @param registry
+ * 
  * @author sabaja
  *
  */
-public class WebConfiiguration implements WebMvcConfigurer{
-	
+public class WebConfiigurationForCORS implements WebMvcConfigurer {
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-	
+
 		WebMvcConfigurer.super.addCorsMappings(registry);
 		registry.addMapping("/**");
 	}

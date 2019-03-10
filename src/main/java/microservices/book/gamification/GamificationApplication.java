41bb9b81d785db11be6afe09df771625ac5162db
sabaja <jacoposabatini76@gmail.com>
2019-03-10 22:41:19 +0100

    @EnableCircuitBreaker

--- src/main/java/microservices/book/gamification/GamificationApplication.java
+++ src/main/java/microservices/book/gamification/GamificationApplication.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,6 +12,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class GamificationApplication {
 
 	public static void main(String[] args) throws IOException {

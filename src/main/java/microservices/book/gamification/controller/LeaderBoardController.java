e918896553826b5cd9673e22d5a697559639cd05
sabaja <jacoposabatini76@gmail.com>
2019-04-25 18:55:38 +0200

    Modify Hystrix Command for LeaderBoardController

--- src/main/java/microservices/book/gamification/controller/LeaderBoardController.java
+++ src/main/java/microservices/book/gamification/controller/LeaderBoardController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,7 +25,14 @@ class LeaderBoardController {
     }
 
     @GetMapping
+    @HystrixCommand(fallbackMethod = "defaultLeader")
     public List<LeaderBoardRow> getLeaderBoard() {
         return leaderBoardService.getCurrentLeaderBoard();
+    }
+    
+    public List<LeaderBoardRow> defaultLeader(){
+    	List<LeaderBoardRow> lbr = new ArrayList<>();
+    	lbr.add(new LeaderBoardRow(1l, 1l));
+    	return lbr;
     }
 }
